e9a9a8bd95314515d23fd8561b7a7d5c50305c74
Anantha Narayanan <anantha.r@launchventures.co>
2024-12-01 12:57:25 +0530

    Update website.py

--- website.py
+++ website.py
@@ -25,10 +25,6 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 
-#agents
-from typing_extensions import List, TypedDict
-from langgraph.graph import StateGraph, START
-
 
 URL = "https://r.jina.ai/https://www.credibila.com/"
 
